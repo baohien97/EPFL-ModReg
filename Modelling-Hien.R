000b52499c9ed7f64147ca136df1622d99532660
Hien <tengsuynh@gmail.com>
2020-06-18 20:58:02 +0200

    fixed code

--- Modelling-Hien.R
+++ Modelling-Hien.R
@@ -8,14 +8,11 @@ CasesByCountry <- unlist(readRDS("CasesByCountry.rds")[[1]])
 CountryPop <- unlist(readRDS("CountryPop.rds")[[1]])
 CountryNames <- readRDS("CountryNames.rds")
 
-#### modelling,glm,gam ####
 
 # We model number of deaths as a fraction of cases confirmed on l previous 
 # days (smoothened)
 
-l_max = 29 # number of previous days to take into account
-#plot.Country("Kuwait", names=CountryNames, deaths=DeathsByCountry, cases=CasesByCountry, pop=CountryPop, plot=T,plot.cumul=T,xmin=50)
-# Kuwait, Qatar, Saudi Arabia and United Arab Emirates
+l_max = 14 # number of previous days to take into account
 
 country = "United_Arab_Emirates"
 deaths.country = (DeathsByCountry[country,])
@@ -23,70 +20,59 @@ cases.country = (CasesByCountry[country,])
 plot(cases.country, deaths.country, main=country, xlab="cases", ylab="deaths")
 abline(lm(deaths.country~cases.country), col="red")
 
-
-deaths.country.cumul = cumsum(DeathsByCountry[country,])
-cases.country.cumul = cumsum(CasesByCountry[country,])
-
 mu.deaths.t = c() 
 mu.cases.t = c()
 
-# mu.deaths.cumul = c()
-# mu.cases.cumul = c()
 
 for (i in (l_max + 1):length(deaths.country)){
-  mu.cases.t[i-l_max] <- log(1+mean(cases.country[(i - l_max): i]))  # plus 1 to avoid log(0)
-  mu.deaths.t[i-l_max] <-log(1+deaths.country[i])
+  mu.cases.t[i-l_max] <- mean(cases.country[(i - l_max): i]) 
+  mu.deaths.t[i-l_max] <- deaths.country[i]
 }
 
 merged = data.frame(mu.deaths.t, mu.cases.t)
-###linear
+#### identity link #### 
 lin.mod <- glm(mu.deaths.t ~ mu.cases.t, data=merged)
 summary(lin.mod)
+AIC(lin.mod)
 par(mfrow=c(1,4)) # Change the panel layout to 1 x 4
 plot(lin.mod)
-###Poisson
-#poiss.mod <- glm(mu.deaths.t ~ mu.cases.t,family=poisson(link="log"), data=merged)
-#summary(poiss.mod)
-#gof(poiss.mod)
-#par(mfrow=c(1,4))
-#plot(poiss.mod)
+plot(predict(lin.mod))
+lines(mu.deaths.t)
 
-#glm negative binomial
-#nb.mod<-glm.nb(mu.deaths.t ~ mu.cases.t,link=log,data=merged)
-#summary(nb.mod)
-#par(mfrow=c(1,4))
-#plot(nb.mod)
-###quasipoisson
-quasi_poiss <- glm(mu.deaths.t ~ mu.cases.t,family = quasipoisson(link="log"),data=merged)
-summary(quasi_poiss)
-anova(quasi_poiss,test="F")
+#### poisson ####
+pois.mod <- glm(mu.deaths.t ~ log(mu.cases.t), family=poisson(link=log), data=merged)
+summary(pois.mod)
+AIC(pois.mod)
+par(mfrow=c(1,4)) # Change the panel layout to 1 x 4
+plot(pois.mod)
+plot(predict(pois.mod))
+lines(mu.deaths.t)
+
+#### quasipoisson #### 
+quasi.poiss <- glm(mu.deaths.t ~ log(mu.cases.t + 1),family = quasipoisson(link="log"),data=merged)
+summary(quasi.poiss)
+AIC(quasi.poiss)
+anova(quasi.poiss,test="F")
 par(mfrow=c(1,4))
 plot(quasi_poiss)
 
-
-###the same for 4 countries together
-deaths.sum <- cumsum(DeathsByCountry["Kuwait",]+ DeathsByCountry["Qatar",]+ DeathsByCountry["United_Arab_Emirates",]+ DeathsByCountry["Saudi_Arabia",])
-cases.sum <- cumsum(CasesByCountry["Kuwait",]+ CasesByCountry["Qatar",]+ CasesByCountry["United_Arab_Emirates",]+ CasesByCountry["Saudi_Arabia",])
-mu.deaths.sum = c()
-mu.cases.sum = c()
-
-for (i in (l_max + 1):length(deaths.sum)){
-  mu.cases.sum[i-l_max] <- log(1+mean(cases.sum[(i - l_max): i]))  
-  mu.deaths.sum[i-l_max] <-log(1+mean(deaths.sum[i]))
-}
-
-merged.more.countries = data.frame(mu.deaths.sum, mu.cases.sum)
-###linear
-lin.mod.more.countries <- glm(mu.deaths.sum ~ mu.cases.sum, data=merged.more.countries)
-summary(lin.mod.more.countries)
-par(mfrow=c(1,4)) # Change the panel layout to 1 x 4
-plot(lin.mod.more.countries)
 
 #### modelling non-linear y= ax/(b+x) ####
 x = mu.cases.t
 mm.mod <- nls(mu.deaths.t ~ a*x/(b+x))
 par(mfrow=c(1,4)) # Change the panel layout to 1 x 4
 plot(predict(mm.mod), mu.deaths.t)
+AIC(mm.mod)
 
 #### modelling gam ####
-
+gam.mod <- gam(mu.deaths.t ~ s(mu.cases.t), family=poisson(link="log"), data = merged) 
+summary(gam.mod)
+par(mfrow=c(1,4))
+plot(gam.mod)
+par(mfrow=c(1,4))
+gam.check(gam.mod)
+qqPlot(residuals(gam.mod))
+#cooks.distance(gam.mod)
+AIC(gam.mod)
+summary(gam.mod)$r.sq
+ggplot(merged, aes(mu.cases.t, mu.deaths.t)) + geom_point() + geom_smooth(method = "gam")
